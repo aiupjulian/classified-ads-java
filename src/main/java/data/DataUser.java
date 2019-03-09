728581e53893e31170ed031435b3ec1b9202d302
Julian Aiup <julian.aiup@leniolabs.com>
2019-03-09 01:03:45 -0300

    Add create and get user

--- src/main/java/data/DataUser.java
+++ src/main/java/data/DataUser.java
@@ -2,9 +2,19 @@ package data;
 
 import java.io.Serializable;
 import java.sql.*;
+import org.mindrot.jbcrypt.BCrypt;
 
 import entity.*;
 import util.AppDataException;
+
+// -------------------------------------
+// Check that an unencrypted password matches one that has
+// previously been hashed
+// if (BCrypt.checkpw(candidate, hashed))
+// 	System.out.println("It matches");
+// else
+// 	System.out.println("It does not match");
+// ---------------------------------------
 
 public class DataUser implements Serializable {
   private static final long serialVersionUID = 1L;
@@ -14,42 +24,42 @@ public class DataUser implements Serializable {
   public void add(User user) {
     ResultSet rs = null;
     PreparedStatement stmt = null;
+    String error = "";
 
     try {
-      // $query = "SELECT * FROM user WHERE username='$username'";
-      // $userResult = mysqli_query($link, $query);
-      // $count = mysqli_num_rows($userResult);
-      // if ($count == 1) {
-      //   $error = "El usuario ya está en uso.";
-      // } else {
-      //   $query = "INSERT INTO user (username, password, name, phone, email) VALUES ('$username', '$hash', '$name', '$phone', '$email')";
-      //   if (mysqli_query($link, $query)) {
-      //     $_SESSION['username'] = $username;
-      //     $_SESSION['id'] = mysqli_insert_id($link);
-      //     $_SESSION['email'] = $email;
-      //     header("location: profile.php");
-      //   } else {
-      //     $error = "Hubo un error al intentar crear el usuario.";
-      //   }
-      // }
-     
       stmt = FactoryConnection.getInstancia().getConn().prepareStatement(
-        "insert into user(dni,nombre, apellido,habilitado)" + " values(?,?,?,?)",
-        PreparedStatement.RETURN_GENERATED_KEYS
+        "SELECT * FROM user WHERE username=?"
       );
-
-      stmt.setInt(1, user.getDni());
-      stmt.setString(2, user.getNombre());
-      stmt.setString(3, user.getApellido());
-      stmt.setBoolean(4, user.isHabilitado());
-      stmt.execute();
-
-      // after executing the insert use the following lines to retrieve the id
-      rs = stmt.getGeneratedKeys();
+      stmt.setString(1, user.getUsername());
+      rs = stmt.executeQuery();
       if (rs != null && rs.next()) {
-        user.setId(rs.getInt(1));
-      }
+        error = "El usuario ya está en uso.";
+      } else {
+        String hash = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+        stmt = FactoryConnection.getInstancia().getConn().prepareStatement(
+          "INSERT INTO user (username, password, name, phone, email) VALUES (?, ?, ?, ?, ?)"
+          PreparedStatement.RETURN_GENERATED_KEYS
+        );
 
+        stmt.setString(1, user.getUsername());
+        stmt.setString(2, hash);
+        stmt.setString(3, user.getName());
+        stmt.setString(4, user.getPhone());
+        stmt.setString(5, user.getEmail());
+        stmt.execute();
+
+        // after executing the insert use the following lines to retrieve the id
+        rs = stmt.getGeneratedKeys();
+        if (rs != null && rs.next()) {
+          user.setId(rs.getInt(1));
+          HttpSession session = request.getSession();
+          session.setAttribute("username", user.getUsername());
+          session.setAttribute("id", user.getId());
+          session.setAttribute("email", user.getEmail());
+          // redirect to profile
+        }
+      }
+// $error = "Hubo un error al intentar crear el usuario.";
     } catch (SQLException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
@@ -73,23 +83,27 @@ public class DataUser implements Serializable {
     }
   }
 
-  public User getById(User per) {
+  public User getById(Int id) {
     User user = null;
-
     PreparedStatement stmt = null;
     ResultSet rs = null;
+
     try {
-      stmt = FactoryConnection.getInstancia().getConn()
-          .prepareStatement("select id, dni, nombre, apellido, habilitado from Users where dni=?");
-      stmt.setInt(1, per.getDni());
+      stmt = FactoryConnection.getInstancia().getConn().prepareStatement(
+        "SELECT * FROM user WHERE id=?"
+      );
+      stmt.setInt(1, id);
       rs = stmt.executeQuery();
       if (rs != null && rs.next()) {
-        user = new User();
-        user.setId(rs.getInt("id"));
-        user.setDni(rs.getInt("dni"));
-        user.setNombre(rs.getString("nombre"));
-        user.setApellido(rs.getString("apellido"));
-        user.setHabilitado(rs.getBoolean("habilitado"));
+        user = new User(
+          id,
+          rs.getString("username"),
+          rs.getString("password"),
+          rs.getString("phone"),
+          rs.getString("email"),
+          rs.getString("name"),
+          rs.getBoolean("admin")
+        );
       }
     } catch (SQLException e) {
       // TODO Auto-generated catch block
